e865aafa1be1314ef7b51ed18a525f0e21effa42
Anjie Wei <anjieWei@protonmail.com>
2023-10-26 01:43:45 +0100

    server configuration

--- server/server.js
+++ server/server.js
@@ -2,9 +2,10 @@ const express= require('express')
 const app = express()
 const cors = require("cors")
 const fetchRoutes = require('./countryFetcher')
+const port = process.env.PORT || 3000;
 
 app.use(cors())
 app.use('/api', fetchRoutes)
 
 
-app.listen(5000, () => {console.log('Server started on port 5000')})
+app.listen(port, () => {console.log('Server started on port 5000')})
