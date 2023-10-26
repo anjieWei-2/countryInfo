ec0acce94bb1733477703c6117235d92a3b35126
Anjie Wei <anjieWei@protonmail.com>
2023-10-26 02:09:40 +0100

    move for deployement

--- server/api/index.js
+++ server/api/index.js
@@ -2,10 +2,10 @@ const express= require('express')
 const app = express()
 const cors = require("cors")
 const fetchRoutes = require('./countryFetcher')
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use('/api', fetchRoutes)
 
 
-app.listen(port, () => {console.log('Server started on port 5000')})
+app.listen(port, () => {console.log(`Server started on port ${port}`)})
