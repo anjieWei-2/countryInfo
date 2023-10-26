92deb418e6f56384c1e0213ee0dfcae2dcd7feac
Anjie Wei <anjieWei@protonmail.com>
2023-10-26 02:50:38 +0100

    .

--- server/api/index.js
+++ server/api/index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use('/api', fetchRoutes)
+app.get('/', (req, res) => res.send('Home Page Route'));
 
 
 app.listen(port, () => {console.log(`Server started on port ${port}`)})
